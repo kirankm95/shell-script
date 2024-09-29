65164bda142569eb40dc508106e600161cd8cc48
Kiran <kirankm95@gmail.com>
2024-09-29 22:18:14 +0530

    adding shell script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2 how are you?"
+echo "$PERSON2: Hi $PERSON1 i am good, thx, how r u ?"
+echo "$PERSON1: I am also super"
+echo "$PERSON2: what'sup ?"
