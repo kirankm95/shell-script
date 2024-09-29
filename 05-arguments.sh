35e0de43294fc2ab307b0d68955621d79d25fc82
Kiran <kirankm95@gmail.com>
2024-09-29 22:20:26 +0530

    adding shell script

---
+++ 05-arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Orey username enter cheyoy:"
+read USERNAME
+
+echo "Orey password kuuda enter cheyroy:"
+read PASSWORD
+
+echo "nv enter chesina username: $USERNAME and password: $PASSWORD"
