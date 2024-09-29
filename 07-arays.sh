e6481d06acf9b700fd7464e6d64f2fe6f61f5fb8
Kiran <kirankm95@gmail.com>
2024-09-29 22:25:21 +0530

    adding shell script

---
+++ 07-arays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DRINKS=("whisky" "beer" "wine" "votka")
+
+echo "winter lo em baguntadho telusa: ${DRINKS[1]}"
+echo "Summer lo em baguntadho telusa: ${DRINKS[2]}"
+
+echo "Em option lekunte edhina Ok ra mama manaki: ${DRINKS[@]}"
