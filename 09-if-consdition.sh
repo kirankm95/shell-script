6387d3762379dfc13a7e977d9451badb2407dd89
Kiran <kirankm95@gmail.com>
2024-10-07 21:38:18 +0530

    new shell script

---
+++ 09-if-consdition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20 ]
+then
+   echo "Orey nv enter chesina number 20 kanna peddhadhi roy"
+else
+   echo "Orey nv enter chesina number 20 kanna chinnadhi ehey"
+fi
