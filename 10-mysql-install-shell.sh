c4ef3b04d750e9a3d24ef4507cc0c7e39b96de83
Kiran <kirankm95@gmail.com>
2024-10-07 21:44:53 +0530

    new shell script

--- 10-mysql-install-shell.sh
+++ 10-mysql-install-shell.sh
@@ -7,4 +7,5 @@ then
    echo "Orey nv root user kadhu, velli root user gadini rammanu, pooo"
 else
    echo "Hello root user, thx for coming, how r u, install chey mari mysql ni"
+   sudo yum install mysql -y
 fi
