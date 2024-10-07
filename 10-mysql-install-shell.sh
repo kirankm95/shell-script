a8716f9df6632804668a85048819874d322cb5db
Kiran <kirankm95@gmail.com>
2024-10-07 21:47:03 +0530

    new shell script

--- 10-mysql-install-shell.sh
+++ 10-mysql-install-shell.sh
@@ -5,7 +5,13 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
    echo "Orey nv root user kadhu, velli root user gadini rammanu, pooo"
+   exit 1
 else
    echo "Hello root user, thx for coming, how r u, install chey mari mysql ni"
    sudo yum install mysql -y
+   if [ $? -eq 0 ]
+   then
+        echo "mysql install is success macha, enjoy pandago"
+    else
+        echo "mysql install is faileddd"
 fi
