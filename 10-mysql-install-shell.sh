05ccb5865ac4d05363f55523328d4a5c34234979
Kiran <kirankm95@gmail.com>
2024-10-07 21:44:14 +0530

    new shell script

---
+++ 10-mysql-install-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+   echo "Orey nv root user kadhu, velli root user gadini rammanu, pooo"
+else
+   echo "Hello root user, thx for coming, how r u, install chey mari mysql ni"
+fi
