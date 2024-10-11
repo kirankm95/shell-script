d173840b7f2a5a6b14e7e6aa74be6229375d79d3
Kiran <kirankm95@gmail.com>
2024-10-11 15:03:00 +0530

    new shell script

--- 06-sum.sh
+++ 06-sum.sh
@@ -6,3 +6,10 @@ secondnumber=$2
 SUM=$(($firstnumber+$secondnumber))
 
 echo "Orey the sum is: $SUM"
+
+echo "how many arguments passed to script: $#"
+echo "all arguments passwd to script: $@"
+echo "first argument passwd: $1"
+echo "script name: $0"
+echo "exit code: $?"
+echo "last argument: $N"
